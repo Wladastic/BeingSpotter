e442c71eef258db8ae3774bf25ed99adc1529a87
Wladastic <wlad.c@me.com>
2020-01-19 21:00:20 +0100

    Initial Commit

---
+++ BeingSpotter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeingSpotter
+//
+//  Created by Wladislav Cugunov on 19.01.20.
+//  Copyright © 2020 Wladislav Cugunov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
